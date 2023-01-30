f9e635f1a4daa20c9b704e1733f19ea7b6abe369
mayurlotanbhoi <mayurbhoi200@gmail.com>
2023-01-30 14:19:59 +0530

    add url and cookies

--- apis/user_apis/user_logic.js
+++ apis/user_apis/user_logic.js
@@ -51,8 +51,8 @@ module.exports = {
           .cookie("jwtoken", jwtoken, {
             httpOnly: true,
             expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-            // sameSite: "Lax",
-            // secure: true,
+            sameSite: "none",
+            secure: true,
           })
           .json({ massege: "Login SuccesFull" });
       }
--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(
+  cors({
+    origin: ["https://car-service-foi1.onrender.com/", "http://localhost:3000"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 const cretUser = require("./apis/user_apis/user_router");
