b04197641506a0da31f95b876e3dd7f0bbc26b40
mayurlotanbhoi <mayurbhoi200@gmail.com>
2023-01-30 15:19:10 +0530

    changr databace url

--- apis/DB_connextion/app_connection.js
+++ apis/DB_connextion/app_connection.js
@@ -1,7 +1,7 @@
 const { createPool } = require("mysql2");
 
 const pool = createPool({
-  host: "localhost",
+  host: "https://car-service-api.onrender.com",
   database: "car_Service_App",
   password: "Golubhoi200@",
   user: "root",
